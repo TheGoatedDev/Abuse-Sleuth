78e0f02755cd1096b23edfc26cc1676076dcdc09
Abyss <32000806+Abys5@users.noreply.github.com>
2022-01-23 18:31:25 +0000

    Added ScanLog API Commenting

--- src/pages/api/v1/scanlog.ts
+++ src/pages/api/v1/scanlog.ts
@@ -18,30 +18,33 @@ const handler = apiHandler.post(
 
         const allValidIPs: string[] = [];
 
+        // Add All Valid IPs to a array
         allEntriesFromBody.forEach((element: string) => {
             if (isIPAddress(element)) {
                 allValidIPs.push(element);
             }
         });
 
+        // Async Scan All IPs and wait for all the result to come back.
         const allPromises: Promise<AIPDBReport | null>[] = [];
-
         allValidIPs.forEach((element) => {
             allPromises.push(scanIP(element, false));
         });
-
         let settled = await Promise.allSettled(allPromises);
 
+        // Check if the Request wants a Report Generated and generated the report.
         if (req.body.generateReport === true) {
+            // Filter out all results that were Rejected
             settled = settled.filter((x) => x != null);
 
+            // Generated the prisma create array from all the results that were resolved.
             const createRecords: { aipdbReportId: number }[] = [];
-
             settled.forEach((element: any) => {
                 const aipdb: AIPDBReport = element.value;
                 createRecords.push({ aipdbReportId: aipdb.id });
             });
 
+            // Create the Report with all the ReportLinks
             const report = await prisma.report.create({
                 data: {
                     reportLinks: {
@@ -57,11 +60,13 @@ const handler = apiHandler.post(
                 },
             });
 
+            // Return the Report ID
             return res.status(200).json({
                 ok: true,
                 data: report.id,
             });
         } else {
+            // Return No Report ID as Request didn't want a report.
             return res.status(200).json({
                 ok: true,
                 data: -1,
