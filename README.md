b42fb0c668fcf55a845e37fc302bc82cea5060cb
Abyss <32000806+Abys5@users.noreply.github.com>
2022-06-11 18:46:11 +0100

    Setup Husky

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ This turborepo uses [Yarn](https://classic.yarnpkg.com/lang/en/) as a package ma
 
 ### Apps and Packages
 
-- `docs`: a [Next.js](https://nextjs.org) app
-- `web`: another [Next.js](https://nextjs.org) app
-- `ui`: a stub React component library shared by both `web` and `docs` applications
-- `eslint-config-custom`: `eslint` configurations (includes `eslint-config-next` and `eslint-config-prettier`)
-- `tsconfig`: `tsconfig.json`s used throughout the monorepo
+-   `docs`: a [Next.js](https://nextjs.org) app
+-   `web`: another [Next.js](https://nextjs.org) app
+-   `ui`: a stub React component library shared by both `web` and `docs` applications
+-   `eslint-config-custom`: `eslint` configurations (includes `eslint-config-next` and `eslint-config-prettier`)
+-   `tsconfig`: `tsconfig.json`s used throughout the monorepo
 
 Each package/app is 100% [TypeScript](https://www.typescriptlang.org/).
 
@@ -20,9 +20,9 @@ Each package/app is 100% [TypeScript](https://www.typescriptlang.org/).
 
 This turborepo has some additional tools already setup for you:
 
-- [TypeScript](https://www.typescriptlang.org/) for static type checking
-- [ESLint](https://eslint.org/) for code linting
-- [Prettier](https://prettier.io) for code formatting
+-   [TypeScript](https://www.typescriptlang.org/) for static type checking
+-   [ESLint](https://eslint.org/) for code linting
+-   [Prettier](https://prettier.io) for code formatting
 
 ## Setup
 
@@ -69,9 +69,9 @@ npx turbo link
 
 Learn more about the power of Turborepo:
 
-- [Pipelines](https://turborepo.org/docs/core-concepts/pipelines)
-- [Caching](https://turborepo.org/docs/core-concepts/caching)
-- [Remote Caching (Beta)](https://turborepo.org/docs/core-concepts/remote-caching)
-- [Scoped Tasks](https://turborepo.org/docs/core-concepts/scopes)
-- [Configuration Options](https://turborepo.org/docs/reference/configuration)
-- [CLI Usage](https://turborepo.org/docs/reference/command-line-reference)
+-   [Pipelines](https://turborepo.org/docs/core-concepts/pipelines)
+-   [Caching](https://turborepo.org/docs/core-concepts/caching)
+-   [Remote Caching (Beta)](https://turborepo.org/docs/core-concepts/remote-caching)
+-   [Scoped Tasks](https://turborepo.org/docs/core-concepts/scopes)
+-   [Configuration Options](https://turborepo.org/docs/reference/configuration)
+-   [CLI Usage](https://turborepo.org/docs/reference/command-line-reference)
--- infra/index.ts
+++ infra/index.ts
@@ -1,112 +1,152 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
+import * as pulumi from "@pulumi/pulumi";
 
-const REGION = digitalocean.Region.LON1
+const config = new pulumi.Config();
+const stack = pulumi.getStack();
 
-const main = async () => {
-    const stack = pulumi.getStack()
+// Config for Global
+const region: digitalocean.Region =
+    config.get("region") ?? digitalocean.Region.LON1;
+const NODE_ENV = stack === "prod" ? "production" : "development";
 
-    // Setup Database
-    const dbCluster = new digitalocean.DatabaseCluster("cluster", {
-        // Postgres 14
-        engine: "pg",
-        version: "14",
+// Config for Database
+const databaseInstance: digitalocean.DatabaseSlug =
+    config.get("database:instance") ?? digitalocean.DatabaseSlug.DB_1VPCU1GB;
+const databaseInstanceCount = config.getNumber("database:instanceCount") ?? 1;
 
-        // London
-        region: REGION,
+// Config for Frontend (NextJs)
+const frontendInstanceSlug = config.get("frontend:instance") ?? "basic-xxs";
+const frontendInstanceCount = config.getNumber("frontend:instanceCount") ?? 1;
 
-        //Cluster Machines
-        size: digitalocean.DatabaseSlug.DB_1VPCU1GB,
-        nodeCount: 1,
-    })
+// Config for backend (Express)
+const backendInstanceSlug = config.get("backend:instance") ?? "basic-xxs";
+const backendInstanceCount = config.getNumber("backend:instanceCount") ?? 1;
 
-    const db = new digitalocean.DatabaseDb("db", {
+// Setup Database
+const dbCluster = new digitalocean.DatabaseCluster("cluster", {
+    name: "abuse-sleuth-db",
+
+    // Postgres 14
+    engine: "pg",
+    version: "14",
+
+    // London
+    region: region,
+
+    //Cluster Machines
+    size: databaseInstance,
+    nodeCount: databaseInstanceCount,
+});
+
+const db = new digitalocean.DatabaseDb("db", {
+    name: "abuse-sleuth",
+    clusterId: dbCluster.id,
+});
+
+// Setup Application
+const app = new digitalocean.App("app", {
+    spec: {
         name: "abuse-sleuth",
-        clusterId: dbCluster.id
-    });
 
-    // Setup Application
-    const app = new digitalocean.App("app", {
-        spec: {
-            name: "abuse-sleuth",
+        region: region,
 
-            region: REGION,
+        domainNames: [
+            {
+                name: "abusesleuth.com",
+                type: "PRIMARY",
+                zone: "abusesleuth.com",
+            },
+        ],
 
-            services: [
+        databases: [
+            {
+                name: "db",
+                clusterName: dbCluster.name,
+                production: NODE_ENV === "production",
+                engine: dbCluster.engine.apply((engine) =>
+                    engine.toUpperCase()
+                ),
+            },
+        ],
 
-                // Next JS Service
-                {
-                    name: "nextjs",
-                    httpPort: 8080,
-                    instanceSizeSlug: "basic-xxs",
-                    github: {
-                        repo: "Abys5/Abuse-Sleuth",
-                        branch: "v2",
-                        deployOnPush: true,
-                    },
-                    routes: [
-                        {
-                            path: "/"
-                        }
-                    ],
-                    buildCommand: "npx turbo run build --scope=\"@abuse-sleuth/web\" --include-dependencies",
-                    runCommand: "yarn workspace @abuse-sleuth/web start"
+        services: [
+            // Next JS Service
+            {
+                name: "nextjs",
+                httpPort: 8080,
+                instanceSizeSlug: frontendInstanceSlug,
+                instanceCount: frontendInstanceCount,
+                github: {
+                    repo: "Abys5/Abuse-Sleuth",
+                    branch: "v2",
+                    deployOnPush: true,
                 },
+                routes: [
+                    {
+                        path: "/",
+                    },
+                ],
+                buildCommand:
+                    'npx turbo run build --scope="@abuse-sleuth/web" --include-dependencies',
+                runCommand: "yarn workspace @abuse-sleuth/web start",
+            },
 
-                // Express API Service
-                // {
-                //     name: "express",
-                //     httpPort: 8080,
-                //     instanceSizeSlug: "basic-xxs",
-                //     github: {
-                //         repo: "Abys5/Abuse-Sleuth",
-                //         branch: "v2",
-                //         deployOnPush: true,
-                //     },
-                //     routes: [
-                //         {
-                //             path: "/api"
-                //         }
-                //     ]
-                // },
-            ]
+            // Express API Service
+            // {
+            //     name: "express",
+            //     httpPort: 8080,
+            //     instanceSizeSlug: "basic-xxs",
+            //     github: {
+            //         repo: "Abys5/Abuse-Sleuth",
+            //         branch: "v2",
+            //         deployOnPush: true,
+            //     },
+            //     routes: [
+            //         {
+            //             path: "/api"
+            //         }
+            //     ]
+            // },
+        ],
+    },
+});
 
-        }
-    })
+// Setup Database Firewall to App
+const firewall = new digitalocean.DatabaseFirewall("database-firewall", {
+    clusterId: dbCluster.id,
+    rules: [
+        {
+            type: "app",
+            value: app.id,
+        },
+    ],
+});
 
+// Setup Project
+// const project = new digitalocean.Project("abuse-sleuth-project", {
+//     name: "Abuse Sleuth",
+//     environment: NODE_ENV,
+//     description: "A SaaS IP and Domain Aggregration and Analysis for Security",
+//     resources: [dbCluster.clusterUrn],
+// });
 
-    // Setup Project
-    try {
-        const project = await digitalocean.getProject({
-            name: "Abuse Sleuth"
-        })
+export const appLiveUrl = app.liveUrl;
 
-        new digitalocean.ProjectResources("db-project", {
-            project: project.id,
-            resources: [
-                dbCluster.clusterUrn,
-                app.urn
-            ]
-        })
-    } catch (error) {
-        new digitalocean.Project("abuse-sleuth", {
-            name: "Abuse Sleuth",
-            resources: [
-                dbCluster.clusterUrn,
-                app.urn
-            ]
-        })
-    }
+// // Setup Project
+// try {
+//     const project = await digitalocean.getProject({
+//         name: "Abuse Sleuth",
+//     });
 
+//     console.log(app.urn.get(), dbCluster.clusterUrn.get());
 
-    return {
-        liveUrl: app.liveUrl
-    }
-
-
-}
-
-
-main().catch((err) => console.error(err));
-
-
+//     new digitalocean.ProjectResources("project-resources", {
+//         project: project.id,
+//         resources: [app.urn, dbCluster.clusterUrn],
+//     });
+// } catch (error) {
+//     new digitalocean.Project("abuse-sleuth-project", {
+//         name: "Abuse Sleuth",
+//         resources: [app.urn, dbCluster.clusterUrn],
+//     });
+// }
