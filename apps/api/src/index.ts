c20a33ae4154bf501b941334a85fa038ad944500
Abys5 <abyss002@pm.me>
2022-06-26 22:44:09 +0100

    feat(web, api): added centralized .env handling

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,10 @@
+import dotENV from "dotenv";
 import { AddressInfo } from "net";
+import path from "path";
 import { fastifyApp } from "server";
+
+const ENVPATH = path.resolve(__dirname, "../../../", ".env");
+const env = dotENV.config({ path: ENVPATH });
 
 const start = async () => {
     try {
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,19 @@ const withTM = require("next-transpile-modules")([
     "@abuse-sleuth/ui",
     "@abuse-sleuth/prisma",
 ]);
+const path = require("path");
+const dotENV = require("dotenv");
+
+const ENVPATH = path.resolve(__dirname, "../../", ".env");
+const env = dotENV.config({ path: ENVPATH });
+
+const parsedEnv = Object.fromEntries(
+    Object.entries(env.parsed).map(([k, v]) =>
+        k.startsWith("NEXT_PUBLIC_") ? [k, v] : [undefined, undefined]
+    )
+);
 
 module.exports = withTM({
+    env: parsedEnv || {},
     reactStrictMode: true,
 });
