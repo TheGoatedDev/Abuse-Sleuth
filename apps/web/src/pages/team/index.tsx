82664804736849cdf445daaf08ca27b9119ff81e
Abys5 <abyss@pm.me>
2022-09-17 21:39:31 +0000

    Created Team Pages (index, create)

---
+++ apps/web/src/pages/team/index.tsx
@@ -0,0 +1,25 @@
+import type { GetServerSideProps, NextPage } from "next";
+
+import { requireAuth } from "@abuse-sleuth/authentication/nextjs";
+import { trpcClient } from "@abuse-sleuth/trpc/nextjs/client";
+import {
+    Group,
+    SimpleGrid,
+    Skeleton,
+    Title,
+} from "@abuse-sleuth/ui/components/atoms";
+import { StatsCard } from "@abuse-sleuth/ui/components/compounds";
+
+import { Layout } from "@components/dashboard/layouts";
+
+const TeamIndex: NextPage = () => {
+    return (
+        <Layout>
+            <Title>Your Teams</Title>
+        </Layout>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = requireAuth();
+
+export default TeamIndex;
