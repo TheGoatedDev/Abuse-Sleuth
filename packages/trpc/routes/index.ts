51795d62a04905fc003423256cd4cc24b69045cc
Abys5 <github.fggvs@slmail.me>
2022-10-06 10:51:49 +0000

    Added Stripe Webhook

--- packages/trpc/routes/index.ts
+++ packages/trpc/routes/index.ts
@@ -1,10 +1,12 @@
 import { trpc } from "../initTRPC";
+import { stripeRouter } from "./stripe";
 import { teamsRouter } from "./teams";
 import { usersRouter } from "./users";
 
 export const appRouter = trpc.router({
     teams: teamsRouter,
     users: usersRouter,
+    stripe: stripeRouter,
 });
 
 export type AppRouter = typeof appRouter;
