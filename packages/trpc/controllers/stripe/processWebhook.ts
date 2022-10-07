6ff777622e61292b6f165b42b60b602b738cb226
Abys5 <github.fggvs@slmail.me>
2022-10-07 10:59:30 +0000

    Added Error Catching on subscription create hook

--- packages/trpc/controllers/stripe/processWebhook.ts
+++ packages/trpc/controllers/stripe/processWebhook.ts
@@ -42,45 +42,51 @@ export const processWebhookController = trpc.procedure
             case "customer.subscription.created":
                 const createdSubscription = event.data
                     .object as Stripe.Subscription;
-                let createdSubTeamId = createdSubscription.metadata["teamId"];
+                try {
+                    let createdSubTeamId =
+                        createdSubscription.metadata["teamId"];
 
-                const team = await prisma.team.findUniqueOrThrow({
-                    where: {
-                        id: createdSubTeamId,
-                    },
-                });
-
-                // Only Adds Stripe Subscription to Team if no exist Subscription
-                if (
-                    team.stripeSubId === null &&
-                    createdSubscription.status === "active"
-                ) {
-                    await prisma.team.update({
-                        where: { id: createdSubTeamId },
-                        data: {
-                            stripeSubId: createdSubscription.id,
+                    const team = await prisma.team.findUniqueOrThrow({
+                        where: {
+                            id: createdSubTeamId,
                         },
                     });
-                    return;
-                }
 
-                // Removes Old Subscription and Add New to Team
-                if (team.stripeSubId) {
-                    await stripe.subscriptions.del(team.stripeSubId);
+                    // Only Adds Stripe Subscription to Team if no exist Subscription
+                    if (
+                        team.stripeSubId === null &&
+                        createdSubscription.status === "active"
+                    ) {
+                        await prisma.team.update({
+                            where: { id: createdSubTeamId },
+                            data: {
+                                stripeSubId: createdSubscription.id,
+                            },
+                        });
+                        return;
+                    }
 
-                    await prisma.team.update({
-                        where: { id: createdSubTeamId },
-                        data: {
-                            stripeSubId: createdSubscription.id,
-                        },
+                    // Removes Old Subscription and Add New to Team
+                    if (team.stripeSubId) {
+                        await stripe.subscriptions.del(team.stripeSubId);
+
+                        await prisma.team.update({
+                            where: { id: createdSubTeamId },
+                            data: {
+                                stripeSubId: createdSubscription.id,
+                            },
+                        });
+                        return;
+                    }
+                } catch (error) {
+                    // If anything fails, to delete the created Subscription
+                    await stripe.subscriptions.del(createdSubscription.id);
+                    throw new TRPCError({
+                        code: "INTERNAL_SERVER_ERROR",
+                        message:
+                            "Error in: customer.subscription.created, Deleting Subscription",
                     });
-                    return;
                 }
-
-                throw new TRPCError({
-                    code: "INTERNAL_SERVER_ERROR",
-                    message: "Unknown Error in: customer.subscription.created",
-                });
 
             case "customer.subscription.updated":
                 const updatedSubscription = event.data
