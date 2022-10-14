e36f5b5bf234bb3a3df27f3962c960e005de28b6
Abys5 <github.fggvs@slmail.me>
2022-10-14 18:30:44 +0100

    NewsSection can now Refresh and load more

--- apps/web/src/components/dashboard/features/NewsSection/index.tsx
+++ apps/web/src/components/dashboard/features/NewsSection/index.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
+
 import { trpcClient } from "@abuse-sleuth/trpc/nextjs/client";
 import {
+    ActionIcon,
     Anchor,
+    Button,
     Group,
     Loader,
     Paper,
+    ScrollArea,
     Stack,
     Text,
     Title,
 } from "@abuse-sleuth/ui/components/atoms";
+import { IconRefresh } from "@abuse-sleuth/ui/icons";
 
+// TODO: It works but dislike the "Load More" functionallity.
 export const NewsSection = () => {
-    const getHackerNews = trpcClient.news.getHackerNews.useQuery({});
+    const [limit, setLimit] = useState<number>(3);
+    const getHackerNews = trpcClient.news.getHackerNews.useQuery({ limit });
 
     if (!getHackerNews.isLoading && !getHackerNews.data) {
         <Paper withBorder p={"xs"}>
@@ -24,7 +32,7 @@ export const NewsSection = () => {
 
     return (
         <>
-            {getHackerNews.isLoading ? (
+            {getHackerNews.isLoading && !getHackerNews.data ? (
                 <Paper withBorder p={"xs"}>
                     <Group align={"center"} grow>
                         <Loader />
@@ -32,25 +40,43 @@ export const NewsSection = () => {
                 </Paper>
             ) : (
                 <Paper withBorder p={"xs"}>
-                    <Title order={2}>News - {getHackerNews.data?.title}</Title>
+                    <Group position="apart">
+                        <Title order={2}>
+                            News - {getHackerNews.data?.title}
+                        </Title>
+                        <ActionIcon
+                            variant="light"
+                            color={"violet"}
+                            onClick={() => getHackerNews.refetch()}>
+                            <IconRefresh />
+                        </ActionIcon>
+                    </Group>
                     <Stack mt={"lg"}>
-                        {getHackerNews.data?.items.map((v, i) => (
-                            <Paper pb={"xs"} key={i}>
-                                <Group>
-                                    <Title order={4}>{v.title}</Title>
-                                    <Text color={"dimmed"} size={"xs"}>
-                                        {new Date(
-                                            v.isoDate as string
-                                        ).toLocaleString()}
+                        <ScrollArea
+                            sx={() => ({
+                                height: "260px",
+                            })}>
+                            {getHackerNews.data?.items.map((v, i) => (
+                                <Paper pb={"xs"} key={i}>
+                                    <Group>
+                                        <Title order={4}>{v.title}</Title>
+                                        <Text color={"dimmed"} size={"xs"}>
+                                            {new Date(
+                                                v.isoDate as string
+                                            ).toLocaleString()}
+                                        </Text>
+                                    </Group>
+                                    <Text>
+                                        {v.contentSnippet}
+                                        {"... "}
+                                        <Anchor href={v.link}>Read more</Anchor>
                                     </Text>
-                                </Group>
-                                <Text>
-                                    {v.contentSnippet}
-                                    {"... "}
-                                    <Anchor href={v.link}>Read more.</Anchor>
-                                </Text>
-                            </Paper>
-                        ))}
+                                </Paper>
+                            ))}
+                        </ScrollArea>
+                        <Button onClick={() => setLimit((old) => old + 3)}>
+                            Load more
+                        </Button>
                     </Stack>
                 </Paper>
             )}
